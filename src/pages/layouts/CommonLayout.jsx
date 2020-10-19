fd6489f0908b299a81107371e0610a4981a7bdf7
Miguel Prada <mapra99@gmail.com>
2020-10-18 21:36:25 -0500

    common layout component

--- src/pages/layouts/CommonLayout.jsx
+++ src/pages/layouts/CommonLayout.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import '../../styles/pages/layouts/CommonLayout.scss';
+import Logo from '../../components/Logo';
+import PageFooter from '../../components/PageFooter';
 
 const CommonLayout = ({ children }) => (
   <div className='main'>
+    <div className='logo-wrap'>
+      <Logo />
+    </div>
     { children }
+    <div className='footer-wrap'>
+      <PageFooter />
+    </div>
   </div>
 );
 
