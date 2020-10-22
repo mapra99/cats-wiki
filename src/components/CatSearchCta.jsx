a50da679b2dba6036fff252b7751ff903376377b
Miguel Prada <mapra99@gmail.com>
2020-10-21 21:41:02 -0500

    search CTA styling

--- src/components/CatSearchCta.jsx
+++ src/components/CatSearchCta.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
 
+import SearchIcon from '../assets/icons/SearchIcon';
+
+import '../styles/components/CatSearchCta.scss';
+
 const CatSearchCta = () => (
-  <div>SEARCH</div>
+  <div className='cat-search-cta'>
+    <div className='input-wrap'>
+      <input type='text' placeholder='Enter your breed' />
+      <button className='button search-button' type='button'>
+        <SearchIcon className='icon search-icon' />
+      </button>
+    </div>
+  </div>
 );
 
 export default CatSearchCta;
