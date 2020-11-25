f127e9c87e2b2a12743cf9a95a741d2398c8665b
Miguel Prada <mapra99@gmail.com>
2020-11-25 01:51:38 -0500

    handling loading and error states

--- src/components/top_breeds/BreedsList.jsx
+++ src/components/top_breeds/BreedsList.jsx
@@ -1,16 +1,14 @@
 import React from 'react';
 
 import {connect} from 'react-redux';
+import {Redirect} from 'react-router';
 import TopBreedsActions from '../../actions/TopBreedsActions';
 
 import BreedDescription from './BreedDescription';
+import SpinnerIcon from '../../assets/icons/SpinnerIcon';
 
 import '../../styles/components/top_breeds/BreedsList.scss';
 class BreedsList extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
   componentDidMount() {
     const {fetchTopBreeds, topResults} = this.props;
     if (topResults.length < 10) {
@@ -36,11 +34,14 @@ class BreedsList extends React.Component {
   }
 
   render() {
-    const {finished} = this.props;
+    const {finished, loading, errors} = this.props;
+
     return (
       <div className="section breeds-list">
         <h1>Top 10 most searched breeds</h1>
         {finished && this.renderList()}
+        {loading && <SpinnerIcon className="loading-icon"/>}
+        {errors && <Redirect to="/error" />}
       </div>
     );
   }
