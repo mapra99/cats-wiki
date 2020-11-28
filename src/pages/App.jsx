9a6e623b6c153ef7472722d1057622dc667def11
Miguel Prada <mapra99@gmail.com>
2020-11-28 17:42:29 -0500

    added component to restore scroll

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import {BrowserRouter, Route} from 'react-router-dom';
 import CommonLayout from './layouts/CommonLayout';
+import ScrollToTop from '../components/shared/ScrollToTop.jsx'
 import Home from './Home';
 import TopBreeds from './TopBreeds';
 import BreedDetails from './BreedDetails';
 
 const App = () => (
   <BrowserRouter>
-    <CommonLayout>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/top-breeds" component={TopBreeds} />
-      <Route exact path="/breeds/:breedId" component={BreedDetails} />
-     </CommonLayout>
+      <CommonLayout>
+        <ScrollToTop />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/top-breeds" component={TopBreeds} />
+        <Route exact path="/breeds/:breedId" component={BreedDetails} />
+      </CommonLayout>
+
   </BrowserRouter>
 );
 
